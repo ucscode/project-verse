bfbee4cc23f469d801aabb782b3bff52800664ba
ucscode <34024404+ucscode@users.noreply.github.com>
2022-09-15 04:03:16 +0100

    project v1.0.3

--- els-core/templates/assets/js/main.js
+++ els-core/templates/assets/js/main.js
@@ -1,1 +1,16 @@
 "use strict";
+
+$(function() {
+
+	$(".hamburger[data-bs-target]").each(function() {
+		let target = this.dataset.bsTarget;
+		let hamburger = this;
+		$( target ).on('hide.bs.collapse', function() {
+			$(hamburger).removeClass('is-active');
+		});
+		$( target ).on('show.bs.collapse', function() {
+			$(hamburger).addClass('is-active');
+		});
+	});
+
+});
