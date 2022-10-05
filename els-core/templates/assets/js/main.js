9ccd86925a41444997d9e8685008e5e2c27463a5
ucscode <34024404+ucscode@users.noreply.github.com>
2022-10-05 11:19:34 +0100

    v1.1.2

--- els-core/templates/assets/js/main.js
+++ els-core/templates/assets/js/main.js
@@ -2,6 +2,8 @@
 
 $(function() {
 
+	//! HAMBURGER;
+	
 	$(".hamburger[data-bs-target]").each(function() {
 		let target = this.dataset.bsTarget;
 		let hamburger = this;
@@ -10,6 +12,21 @@ $(function() {
 		});
 		$( target ).on('show.bs.collapse', function() {
 			$(hamburger).addClass('is-active');
+		});
+	});
+	
+	//! SELECT - OPTION;
+	
+	$("select[value]").each(function() {
+		let self = this;
+		let value = self.getAttribute('value');
+		$(self).find('option').each(function(i) {
+			if( this.hasAttribute('value') ) var option = this.getAttribute('value');
+			else var option = this.innerText.trim();
+			if( option == value ) {
+				self.selectedIndex = i;
+				self.dispatchEvent(new Event('change'));
+			}
 		});
 	});
 
